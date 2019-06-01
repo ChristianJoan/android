256facd869a8e26df19a4cfe63f32ef0bf92f542
ChristianJoan <joan.dimitri@hotmail.com>
2019-06-01 02:40:11 -0500

    Juego 4 y 5
    Actualización de display game 4 y 5

--- app/src/main/java/com/angel/alois4/DisplayGame4.java
+++ app/src/main/java/com/angel/alois4/DisplayGame4.java
@@ -1,13 +1,148 @@
 package com.angel.alois4;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DisplayGame4 extends Activity {
+
+    String problema[] = {"lluvia","lentes","cacao"};
+    String correcto[] = {"nube","ojos","taza"};
+    String incorrecto[] = {"sol","nariz","vaso"};
+    int acierto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_game4);
+
+        start();
+
+        ImageView bien = (ImageView) findViewById(R.id.bien);
+        bien.setVisibility(View.INVISIBLE);
+        ImageButton b1 = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton b2 = (ImageButton) findViewById(R.id.imageButton2);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageButton b1 = (ImageButton) findViewById(R.id.imageButton);
+                ImageButton b2 = (ImageButton) findViewById(R.id.imageButton2);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                if (acierto == 1){
+                    bien.setImageResource(R.drawable.acierto);
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                ImageButton b1 = (ImageButton) findViewById(R.id.imageButton);
+                ImageButton b2 = (ImageButton) findViewById(R.id.imageButton2);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+
+                if (acierto == 2){
+                    bien.setImageResource(R.drawable.acierto);
+
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+
+        b1.setEnabled(true);
+        b2.setEnabled(true);
+    }
+
+    public void start(){
+
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+
+        ImageButton b1 = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton b2 = (ImageButton) findViewById(R.id.imageButton2);
+
+        int numero = (int) (Math.random() * problema.length) + 1;
+        acierto = (int) (Math.random() * 2) + 1;
+
+        switch (numero){
+            case 1:
+                img.setImageResource(R.drawable.lluvia);
+                if(acierto == 1){
+                    b1.setImageResource(R.drawable.nube);
+                    b2.setImageResource(R.drawable.sol);
+                } else {
+                    b1.setImageResource(R.drawable.sol);
+                    b2.setImageResource(R.drawable.nube);
+                }
+                break;
+
+            case 2:
+                img.setImageResource(R.drawable.lentes);
+                if(acierto == 1){
+                    b1.setImageResource(R.drawable.ojos);
+                    b2.setImageResource(R.drawable.boca);
+                } else {
+                    b1.setImageResource(R.drawable.boca);
+                    b2.setImageResource(R.drawable.ojos);
+                }
+                break;
+
+
+            case 3:
+                img.setImageResource(R.drawable.cacao);
+                if(acierto == 1){
+                    b1.setImageResource(R.drawable.taza);
+                    b2.setImageResource(R.drawable.vaso);
+                } else {
+                    b1.setImageResource(R.drawable.vaso);
+                    b2.setImageResource(R.drawable.taza);
+                }
+                break;
+        }
+        b1.setEnabled(true);
+        b2.setEnabled(true);
     }
 }
--- app/src/main/java/com/angel/alois4/DisplayGame5.java
+++ app/src/main/java/com/angel/alois4/DisplayGame5.java
@@ -2,12 +2,291 @@ package com.angel.alois4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DisplayGame5 extends Activity {
+
+    String preguntas[] = {"El Limon","El Aguacate","El Mar","El Cielo","El Platano","El Girasol","La Sangre", "La Cereza", "La Tierra", "El café", "La zanahoria","La Mandarina"};
+    String respuesta[] = {"1","1","2","2","3","3","4","4","5","5","6","6"};
+    String botonCorrecto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_game5);
+
+        start();
+        ImageView bien = (ImageView) findViewById(R.id.bien);
+        bien.setVisibility(View.INVISIBLE);
+        Button b1 = (Button) findViewById(R.id.button1);
+        Button b2 = (Button) findViewById(R.id.button2);
+        Button b3 = (Button) findViewById(R.id.button3);
+        Button b4 = (Button) findViewById(R.id.button4);
+        Button b5 = (Button) findViewById(R.id.button5);
+        Button b6 = (Button) findViewById(R.id.button6);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b1 = (Button) findViewById(R.id.button1);
+                Button b2 = (Button) findViewById(R.id.button2);
+                Button b3 = (Button) findViewById(R.id.button3);
+                Button b4 = (Button) findViewById(R.id.button4);
+                Button b5 = (Button) findViewById(R.id.button5);
+                Button b6 = (Button) findViewById(R.id.button6);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                if (botonCorrecto == "1"){
+                    bien.setImageResource(R.drawable.acierto);
+
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b1 = (Button) findViewById(R.id.button1);
+                Button b2 = (Button) findViewById(R.id.button2);
+                Button b3 = (Button) findViewById(R.id.button3);
+                Button b4 = (Button) findViewById(R.id.button4);
+                Button b5 = (Button) findViewById(R.id.button5);
+                Button b6 = (Button) findViewById(R.id.button6);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                if (botonCorrecto == "2"){
+                    bien.setImageResource(R.drawable.acierto);
+
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b1 = (Button) findViewById(R.id.button1);
+                Button b2 = (Button) findViewById(R.id.button2);
+                Button b3 = (Button) findViewById(R.id.button3);
+                Button b4 = (Button) findViewById(R.id.button4);
+                Button b5 = (Button) findViewById(R.id.button5);
+                Button b6 = (Button) findViewById(R.id.button6);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                if (botonCorrecto == "3"){
+                    bien.setImageResource(R.drawable.acierto);
+
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b1 = (Button) findViewById(R.id.button1);
+                Button b2 = (Button) findViewById(R.id.button2);
+                Button b3 = (Button) findViewById(R.id.button3);
+                Button b4 = (Button) findViewById(R.id.button4);
+                Button b5 = (Button) findViewById(R.id.button5);
+                Button b6 = (Button) findViewById(R.id.button6);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                if (botonCorrecto == "4"){
+                    bien.setImageResource(R.drawable.acierto);
+
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b1 = (Button) findViewById(R.id.button1);
+                Button b2 = (Button) findViewById(R.id.button2);
+                Button b3 = (Button) findViewById(R.id.button3);
+                Button b4 = (Button) findViewById(R.id.button4);
+                Button b5 = (Button) findViewById(R.id.button5);
+                Button b6 = (Button) findViewById(R.id.button6);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                if (botonCorrecto == "5"){
+                    bien.setImageResource(R.drawable.acierto);
+
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b1 = (Button) findViewById(R.id.button1);
+                Button b2 = (Button) findViewById(R.id.button2);
+                Button b3 = (Button) findViewById(R.id.button3);
+                Button b4 = (Button) findViewById(R.id.button4);
+                Button b5 = (Button) findViewById(R.id.button5);
+                Button b6 = (Button) findViewById(R.id.button6);
+
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+
+                ImageView bien = (ImageView) findViewById(R.id.bien);
+                if (botonCorrecto == "6"){
+                    bien.setImageResource(R.drawable.acierto);
+
+                } else{
+                    bien.setImageResource(R.drawable.error);
+                }
+                bien.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView bien = (ImageView) findViewById(R.id.bien);
+                        bien.setVisibility(View.INVISIBLE);
+                        start();
+                    }
+                },800);
+
+
+            }
+        });
+
+
+
+    }
+
+    public void start(){
+        TextView pregunta = (TextView) findViewById(R.id.pregunta);
+        int numero = (int) (Math.random() * preguntas.length) + 1;
+        botonCorrecto = respuesta[numero-1];
+        pregunta.setText(preguntas[numero-1]);
+
+        Button b1 = (Button) findViewById(R.id.button1);
+        Button b2 = (Button) findViewById(R.id.button2);
+        Button b3 = (Button) findViewById(R.id.button3);
+        Button b4 = (Button) findViewById(R.id.button4);
+        Button b5 = (Button) findViewById(R.id.button5);
+        Button b6 = (Button) findViewById(R.id.button6);
+
+        b1.setEnabled(true);
+        b2.setEnabled(true);
+        b3.setEnabled(true);
+        b4.setEnabled(true);
+        b5.setEnabled(true);
+        b6.setEnabled(true);
     }
 }
